7004ef6b727b9669f9f7053b58b8c9d6adf6242b
georgemakrakis <georgemakrakis88@gmail.com>
2022-11-23 21:58:47 -0700

    Added lua poc

--- load-balancer-openresty/rewrite-upstream_2.lua
+++ load-balancer-openresty/rewrite-upstream_2.lua
@@ -1,48 +1,17 @@
 local balancer = require("ngx.balancer")
 local server = "172.20.0.3:80"
 
-
-local function splitByChunk(assignments, chunkSize)
-    -- our current index
-   local i = 1
-   -- our chunk counter
-   local count = 0
-   return function()
-     -- abort if we reached the end of lst
-     if i > #assignments then return end
-     -- get a slice of lst
-     local chunk = table.move(assignments, i, i + chunkSize -1, 1, {})
-     -- next starting index
-     i = i + chunkSize
-     count = count + 1
-     return count, chunk
-   end
-end
-
-local server_assignments = {
-    ["172.20.0.3:80"] = {},
-    ["172.20.0.4:80"] = {},
-}
-
--- local clients = {}
--- Check if the client already exists and if so evict it and assign again
--- table.insert(clients, new_client)
-
-clients[ngx.var.remote_addr] = ngx.var.remote_addr
-
-local splits = 2
-for i, chunk in splitByChunk(clients, splits) do
-    if i == 1 then
-        local key, value = next(a, nil)
-        server_assignments[key] = chunk;
-    elseif i > 1 then
-        local key, value = next(a, i-1)
-        server_assignments[key] = chunk;
-    end
-end
-
 if ngx.var.remote_addr == "172.20.0.6"  then
-    server = "172.20.0.3:80"
+    -- server = "172.20.0.4:80"
+    -- ngx.shared.request_counters:set("First", 1)
+    if ngx.shared.request_counters:get("First") == 1 then
+        server = "172.20.0.4:80"
+       ngx.shared.request_counters:set("First", 2)
+    --end
+    elseif ngx.shared.request_counters:get("First") == 2 then
+        server = "172.20.0.3:80"
+        ngx.shared.request_counters:set("First", 1)
+    end
 end
 
 if ngx.var.remote_addr == "172.20.0.5"  then
